d56e81b50495be8b9017dba25f2b1013bab1d129
Batli Joselevitz <batlijoselevitz@Batlis-MacBook-Pro.local>
2017-12-07 22:52:46 -0600

    variables

---
+++ part1.js
@@ -0,0 +1,7 @@
+var words = ["Sugar, spice, everything, nice"];
+var letters = ["A","B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+var wins = 0;
+var losses = 0;
+var userGuess = null;
+var wordLetters = [];
